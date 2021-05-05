b7dc7dfcc79948eb4e788ffe8f1331cf5a037e63
nanavuletic <nenad.vuletic@wolkabout.com>
2021-05-05 14:25:31 +0200

    Reformat

--- core/utilities/LogManager.cpp
+++ core/utilities/LogManager.cpp
@@ -196,8 +196,7 @@ std::vector<std::string> LogManager::getLogsToDelete()
     logFiles = getLogFileNames();
 
     logFiles.erase(std::remove_if(logFiles.begin(), logFiles.end(),
-                                  [&](const std::string& file) -> bool
-                                  {
+                                  [&](const std::string& file) -> bool {
                                       return std::chrono::duration<double>(std::difftime(
                                                std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()),
                                                FileSystemUtils::getLastModified(
@@ -228,15 +227,15 @@ std::vector<std::string> LogManager::getLogsToUpload()
 
     std::vector<std::string> remoteFiles = m_logUploader->getRemoteLogs();
 
-    logFiles.erase(
-      std::remove_if(logFiles.begin(), logFiles.end(),
-                     [&](const std::string& file) -> bool
-                     { return std::find(remoteFiles.begin(), remoteFiles.end(), file) != remoteFiles.end(); }),
-      logFiles.end());
+    logFiles.erase(std::remove_if(logFiles.begin(), logFiles.end(),
+                                  [&](const std::string& file) -> bool {
+                                      return std::find(remoteFiles.begin(), remoteFiles.end(), file) !=
+                                             remoteFiles.end();
+                                  }),
+                   logFiles.end());
 
     logFiles.erase(std::remove_if(logFiles.begin(), logFiles.end(),
-                                  [&](const std::string& file) -> bool
-                                  {
+                                  [&](const std::string& file) -> bool {
                                       return std::chrono::duration<double>(std::difftime(
                                                std::chrono::system_clock::to_time_t(std::chrono::system_clock::now()),
                                                FileSystemUtils::getLastModified(
@@ -253,8 +252,9 @@ std::vector<std::string> LogManager::getLogFileNames()
 
     logFiles = FileSystemUtils::listFiles(m_logDirectory);
     logFiles.erase(std::remove_if(logFiles.begin(), logFiles.end(),
-                                  [&](const std::string& file) -> bool
-                                  { return !wolkabout::StringUtils::endsWith(file, m_logExtension); }),
+                                  [&](const std::string& file) -> bool {
+                                      return !wolkabout::StringUtils::endsWith(file, m_logExtension);
+                                  }),
                    logFiles.end());
     return logFiles;
 }
@@ -360,8 +360,9 @@ void LogManager::checkLogOverflow()
         }
 
         std::sort(logsWithAge.begin(), logsWithAge.end(),
-                  [](const std::pair<std::string, int>& left, const std::pair<std::string, int>& right)
-                  { return left.second > right.second; });
+                  [](const std::pair<std::string, int>& left, const std::pair<std::string, int>& right) {
+                      return left.second > right.second;
+                  });
 
         const std::string oldLog = logsWithAge.begin()->first;
 
