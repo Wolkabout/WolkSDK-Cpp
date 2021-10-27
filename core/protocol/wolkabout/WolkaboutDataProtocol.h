5e788c8c45c3e4ec51bac092beb6514d08d08a12
LazarBozic94 <lazar.bozic@wolkabout.com>
2021-10-27 08:57:12 +0200

    Added DataProtocol abstraction

--- core/protocol/wolkabout/WolkaboutDataProtocol.h
+++ core/protocol/wolkabout/WolkaboutDataProtocol.h
@@ -1,7 +1,7 @@
 #ifndef WOLKABOUTCORE_WOLKABOUTDATAPROTOCOL_H
 #define WOLKABOUTCORE_WOLKABOUTDATAPROTOCOL_H
 #include "core/model/Message.h"
-#include "core/protocol/Protocol.h"
+#include "core/protocol/DataProtocol.h"
 
 #include <string>
 
@@ -14,27 +14,27 @@ const std::string CHANNEL_SINGLE_LEVEL_WILDCARD = "+";
 const std::string DEVICE_TO_PLATFORM_DIRECTION = "d2p/";
 const std::string PLATFORM_TO_DEVICE_DIRECTION = "p2d/";
 
-class WolkaboutDataProtocol : public Protocol
+class WolkaboutDataProtocol : public DataProtocol
 {
 private:
     std::unique_ptr<MessageObject> feedValuesFromContent(std::string content);
     std::unique_ptr<MessageObject> parametersFromContent(std::string content);
 public:
     std::unique_ptr<Message> makeOutboundMessage(const std::string& deviceKey,
-                                                 FeedRegistrationMessage feedRegistrationMessage);
-    std::unique_ptr<Message> makeOutboundMessage(const std::string& deviceKey, FeedRemovalMessage feedRemovalMessage);
-    std::unique_ptr<Message> makeOutboundMessage(const std::string& deviceKey, FeedValuesMessage feedValuesMessage);
+                                                 FeedRegistrationMessage feedRegistrationMessage) override;
+    std::unique_ptr<Message> makeOutboundMessage(const std::string& deviceKey, FeedRemovalMessage feedRemovalMessage) override;
+    std::unique_ptr<Message> makeOutboundMessage(const std::string& deviceKey, FeedValuesMessage feedValuesMessage) override;
     std::unique_ptr<Message> makeOutboundMessage(const std::string& deviceKey,
-                                                 PullFeedValuesMessage pullFeedValuesMessage);
+                                                 PullFeedValuesMessage pullFeedValuesMessage) override;
 
     std::unique_ptr<Message> makeOutboundMessage(const std::string& deviceKey,
-                                                 AttributeRegistrationMessage attributeRegistrationMessage);
+                                                 AttributeRegistrationMessage attributeRegistrationMessage) override;
     std::unique_ptr<Message> makeOutboundMessage(const std::string& deviceKey,
-                                                 ParametersUpdateMessage parametersUpdateMessage);
+                                                 ParametersUpdateMessage parametersUpdateMessage) override;
     std::unique_ptr<Message> makeOutboundMessage(const std::string& deviceKey,
-                                                 ParametersPullMessage parametersPullMessage);
+                                                 ParametersPullMessage parametersPullMessage) override;
 
-    std::unique_ptr<MessageObject> parseInboundMessage(const Message& reference);
+    std::unique_ptr<MessageObject> parseInboundMessage(const Message& reference) override;
     std::vector<std::string> getInboundChannelsForDevice(const std::string& deviceKey) const override;
     std::string extractDeviceKeyFromChannel(const std::string& topic) const override;
 };
