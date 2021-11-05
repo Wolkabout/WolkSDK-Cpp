7e18484225dfe6194e1d4173893cf9225b03784f
LazarBozic94 <lazar.bozic@wolkabout.com>
2021-11-05 15:06:10 +0100

    Naming fix

--- core/model/Device.cpp
+++ core/model/Device.cpp
@@ -6,58 +6,69 @@ const std::string& wolkabout::Device::getName() const
 {
     return m_name;
 }
-void wolkabout::Device::setName(const std::string& mName)
+void wolkabout::Device::setName(const std::string& name)
 {
-    m_name = mName;
+    m_name = name;
 }
 const std::string& wolkabout::Device::getKey() const
 {
     return m_key;
 }
-void wolkabout::Device::setKey(const std::string& mKey)
+void wolkabout::Device::setKey(const std::string& key)
 {
-    m_key = mKey;
+    m_key = key;
 }
 const std::string& wolkabout::Device::getPassword() const
 {
     return m_password;
 }
-void wolkabout::Device::setPassword(const std::string& mPassword)
+void wolkabout::Device::setPassword(const std::string& password)
 {
-    m_password = mPassword;
+    m_password = password;
 }
 const std::vector<Feed>& wolkabout::Device::getFeeds() const
 {
     return m_feeds;
 }
-void wolkabout::Device::setFeeds(const std::vector<Feed>& mFeeds)
+void wolkabout::Device::setFeeds(const std::vector<Feed>& feeds)
 {
-    m_feeds = mFeeds;
+    m_feeds = feeds;
 }
 const std::vector<Attribute>& wolkabout::Device::getAttributes() const
 {
     return m_attributes;
 }
-void wolkabout::Device::setAttributes(const std::vector<Attribute>& mAttributes)
+void wolkabout::Device::setAttributes(const std::vector<Attribute>& attributes)
 {
-    m_attributes = mAttributes;
+    m_attributes = attributes;
 }
 const std::map<ParameterName, std::string>& wolkabout::Device::getParameters() const
 {
     return m_parameters;
 }
-void wolkabout::Device::setParameters(const std::map<ParameterName, std::string>& mParameters)
+void wolkabout::Device::setParameters(const std::map<ParameterName, std::string>& parameters)
 {
-    m_parameters = mParameters;
+    m_parameters = parameters;
 }
-wolkabout::Device::Device(std::string key, std::string password, std::shared_ptr<DeviceTemplate> deviceTemplate)
-: m_key(std::move(key)), m_password(std::move(password)), m_name("")
+OutboundDataMode Device::getType() const
+{
+    return m_type;
+}
+void Device::setType(OutboundDataMode type)
+{
+    m_type = type;
+}
+wolkabout::Device::Device(std::string key, std::string password, OutboundDataMode type, std::shared_ptr<DeviceTemplate> deviceTemplate)
+: m_name("")
+, m_key(std::move(key))
+, m_password(std::move(password))
+, m_type(type)
 {
     // TODO implement reading from device template file
 }
 wolkabout::Device::Device(std::string name, std::string key, std::string password,
-                          std::shared_ptr<DeviceTemplate> deviceTemplate)
-: m_name(std::move(name)), m_key(std::move(key)), m_password(std::move(password))
+                          OutboundDataMode type, std::shared_ptr<DeviceTemplate> deviceTemplate)
+: m_name(std::move(name)), m_key(std::move(key)), m_password(std::move(password)), m_type(type)
 {
     // TODO implement reading from device template file
 }
