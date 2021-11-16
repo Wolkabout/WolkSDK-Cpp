9bff193dccf3ec41e02ab3e4500fcf37791f156e
nanavuletic <nenad.vuletic@wolkabout.com>
2021-11-16 16:16:49 +0100

    Added the pre-commit script

--- core/Types.cpp
+++ core/Types.cpp
@@ -1,7 +1,8 @@
-#include "Types.h"
+#include "core/Types.h"
 
 #include <algorithm>
 #include <sstream>
+
 namespace wolkabout
 {
 const char DELIMITER = ',';
@@ -31,19 +32,19 @@ std::string toString(DataType type)
 
 DataType dataTypeFromString(const std::string& type)
 {
-    if(type == "BOOLEAN")
+    if (type == "BOOLEAN")
         return DataType::BOOLEAN;
-    if(type == "NUMERIC")
+    if (type == "NUMERIC")
         return DataType::NUMERIC;
-    if(type == "STRING")
+    if (type == "STRING")
         return DataType::STRING;
-    if(type == "HEXADECIMAL")
+    if (type == "HEXADECIMAL")
         return DataType::HEXADECIMAL;
-    if(type == "LOCATION")
+    if (type == "LOCATION")
         return DataType::LOCATION;
-    if(type == "ENUM")
+    if (type == "ENUM")
         return DataType::ENUM;
-    if(type == "VECTOR")
+    if (type == "VECTOR")
         return DataType::VECTOR;
     return DataType::UNKNOWN;
 }
@@ -63,9 +64,9 @@ std::string toString(FeedType type)
 
 FeedType feedTypeFromString(const std::string& type)
 {
-    if(type == "IN")
+    if (type == "IN")
         return FeedType::IN;
-    if(type == "IN/OUT")
+    if (type == "IN/OUT")
         return FeedType::IN_OUT;
 
     return FeedType::IN;
@@ -336,259 +337,259 @@ std::string toString(Unit unit)
 
 Unit unitFromString(const std::string& type)
 {
-    if(type == "NUMERIC")
+    if (type == "NUMERIC")
         return Unit::NUMERIC;
-    if(type == "BOOLEAN")
+    if (type == "BOOLEAN")
         return Unit::BOOLEAN;
-    if(type == "PERCENT")
+    if (type == "PERCENT")
         return Unit::PERCENT;
-    if(type == "DECIBEL")
+    if (type == "DECIBEL")
         return Unit::DECIBEL;
-    if(type == "LOCATION")
+    if (type == "LOCATION")
         return Unit::LOCATION;
-    if(type == "METRES_PER_SQUARE_SECOND")
+    if (type == "METRES_PER_SQUARE_SECOND")
         return Unit::METRES_PER_SQUARE_SECOND;
-    if(type == "G")
+    if (type == "G")
         return Unit::G;
-    if(type == "MOLE")
+    if (type == "MOLE")
         return Unit::MOLE;
-    if(type == "ATOM")
+    if (type == "ATOM")
         return Unit::ATOM;
-    if(type == "RADIAN")
+    if (type == "RADIAN")
         return Unit::RADIAN;
-    if(type == "REVOLUTION")
+    if (type == "REVOLUTION")
         return Unit::REVOLUTION;
-    if(type == "DEGREE_ANGLE")
+    if (type == "DEGREE_ANGLE")
         return Unit::DEGREE_ANGLE;
-    if(type == "MINUTE_ANGLE")
+    if (type == "MINUTE_ANGLE")
         return Unit::MINUTE_ANGLE;
-    if(type == "SECOND_ANGLE")
+    if (type == "SECOND_ANGLE")
         return Unit::SECOND_ANGLE;
-    if(type == "CENTIRADIAN")
+    if (type == "CENTIRADIAN")
         return Unit::CENTIRADIAN;
-    if(type == "GRADE")
+    if (type == "GRADE")
         return Unit::GRADE;
-    if(type == "SQUARE_METRE")
+    if (type == "SQUARE_METRE")
         return Unit::SQUARE_METRE;
-    if(type == "ARE")
+    if (type == "ARE")
         return Unit::ARE;
-    if(type == "HECTARE")
+    if (type == "HECTARE")
         return Unit::HECTARE;
-    if(type == "KATAL")
+    if (type == "KATAL")
         return Unit::KATAL;
-    if(type == "BIT")
+    if (type == "BIT")
         return Unit::BIT;
-    if(type == "BYTE")
+    if (type == "BYTE")
         return Unit::BYTE;
-    if(type == "SECOND")
+    if (type == "SECOND")
         return Unit::SECOND;
-    if(type == "MINUTE")
+    if (type == "MINUTE")
         return Unit::MINUTE;
-    if(type == "HOUR")
+    if (type == "HOUR")
         return Unit::HOUR;
-    if(type == "DAY")
+    if (type == "DAY")
         return Unit::DAY;
-    if(type == "WEEK")
+    if (type == "WEEK")
         return Unit::WEEK;
-    if(type == "YEAR")
+    if (type == "YEAR")
         return Unit::YEAR;
-    if(type == "MONTH")
+    if (type == "MONTH")
         return Unit::MONTH;
-    if(type == "DAY_SIDEREAL")
+    if (type == "DAY_SIDEREAL")
         return Unit::DAY_SIDEREAL;
-    if(type == "YEAR_SIDEREAL")
+    if (type == "YEAR_SIDEREAL")
         return Unit::YEAR_SIDEREAL;
-    if(type == "YEAR_CALENDAR")
+    if (type == "YEAR_CALENDAR")
         return Unit::YEAR_CALENDAR;
-    if(type == "POISE")
+    if (type == "POISE")
         return Unit::POISE;
-    if(type == "FARAD")
+    if (type == "FARAD")
         return Unit::FARAD;
-    if(type == "COULOMB")
+    if (type == "COULOMB")
         return Unit::COULOMB;
-    if(type == "E")
+    if (type == "E")
         return Unit::E;
-    if(type == "FARADAY")
+    if (type == "FARADAY")
         return Unit::FARADAY;
-    if(type == "FRANKLIN")
+    if (type == "FRANKLIN")
         return Unit::FRANKLIN;
-    if(type == "SIEMENS")
+    if (type == "SIEMENS")
         return Unit::SIEMENS;
-    if(type == "AMPERE")
+    if (type == "AMPERE")
         return Unit::AMPERE;
-    if(type == "GILBERT")
+    if (type == "GILBERT")
         return Unit::GILBERT;
-    if(type == "HENRY")
+    if (type == "HENRY")
         return Unit::HENRY;
-    if(type == "VOLT")
+    if (type == "VOLT")
         return Unit::VOLT;
-    if(type == "CENTIVOLT")
+    if (type == "CENTIVOLT")
         return Unit::CENTIVOLT;
-    if(type == "MILLIVOLT")
+    if (type == "MILLIVOLT")
         return Unit::MILLIVOLT;
-    if(type == "OHM")
+    if (type == "OHM")
         return Unit::OHM;
-    if(type == "JOULE")
+    if (type == "JOULE")
         return Unit::JOULE;
-    if(type == "ERG")
+    if (type == "ERG")
         return Unit::ERG;
-    if(type == "ELECTRON_VOLT")
+    if (type == "ELECTRON_VOLT")
         return Unit::ELECTRON_VOLT;
-    if(type == "NEWTON")
+    if (type == "NEWTON")
         return Unit::NEWTON;
-    if(type == "DYNE")
+    if (type == "DYNE")
         return Unit::DYNE;
-    if(type == "KILOGRAM_FORCE")
+    if (type == "KILOGRAM_FORCE")
         return Unit::KILOGRAM_FORCE;
-    if(type == "POUND_FORCE")
+    if (type == "POUND_FORCE")
         return Unit::POUND_FORCE;
-    if(type == "HERTZ")
+    if (type == "HERTZ")
         return Unit::HERTZ;
-    if(type == "MEGAHERTZ")
+    if (type == "MEGAHERTZ")
         return Unit::MEGAHERTZ;
-    if(type == "GIGAHERTZ")
+    if (type == "GIGAHERTZ")
         return Unit::GIGAHERTZ;
-    if(type == "LUX")
+    if (type == "LUX")
         return Unit::LUX;
-    if(type == "LAMBERT")
+    if (type == "LAMBERT")
         return Unit::LAMBERT;
-    if(type == "STOKE")
+    if (type == "STOKE")
         return Unit::STOKE;
-    if(type == "METRE")
+    if (type == "METRE")
         return Unit::METRE;
-    if(type == "KILOMETRE")
+    if (type == "KILOMETRE")
         return Unit::KILOMETRE;
-    if(type == "CENTIMETRE")
+    if (type == "CENTIMETRE")
         return Unit::CENTIMETRE;
-    if(type == "MILLIMETRE")
+    if (type == "MILLIMETRE")
         return Unit::MILLIMETRE;
-    if(type == "FOOT")
+    if (type == "FOOT")
         return Unit::FOOT;
-    if(type == "FOOT_SURVEY_US")
+    if (type == "FOOT_SURVEY_US")
         return Unit::FOOT_SURVEY_US;
-    if(type == "YARD")
+    if (type == "YARD")
         return Unit::YARD;
-    if(type == "INCH")
+    if (type == "INCH")
         return Unit::INCH;
-    if(type == "MILE")
+    if (type == "MILE")
         return Unit::MILE;
-    if(type == "NAUTICAL_MILE")
+    if (type == "NAUTICAL_MILE")
         return Unit::NAUTICAL_MILE;
-    if(type == "ANGSTROM")
+    if (type == "ANGSTROM")
         return Unit::ANGSTROM;
-    if(type == "ASTRONOMICAL_UNIT")
+    if (type == "ASTRONOMICAL_UNIT")
         return Unit::ASTRONOMICAL_UNIT;
-    if(type == "LIGHT_YEAR")
+    if (type == "LIGHT_YEAR")
         return Unit::LIGHT_YEAR;
-    if(type == "PARSEC")
+    if (type == "PARSEC")
         return Unit::PARSEC;
-    if(type == "POINT")
+    if (type == "POINT")
         return Unit::POINT;
-    if(type == "PIXEL")
+    if (type == "PIXEL")
         return Unit::PIXEL;
-    if(type == "LUMEN")
+    if (type == "LUMEN")
         return Unit::LUMEN;
-    if(type == "CANDELA")
+    if (type == "CANDELA")
         return Unit::CANDELA;
-    if(type == "WEBER")
+    if (type == "WEBER")
         return Unit::WEBER;
-    if(type == "MAXWELL")
+    if (type == "MAXWELL")
         return Unit::MAXWELL;
-    if(type == "TESLA")
+    if (type == "TESLA")
         return Unit::TESLA;
-    if(type == "GAUSS")
+    if (type == "GAUSS")
         return Unit::GAUSS;
-    if(type == "KILOGRAM")
+    if (type == "KILOGRAM")
         return Unit::KILOGRAM;
-    if(type == "GRAM")
+    if (type == "GRAM")
         return Unit::GRAM;
-    if(type == "ATOMIC_MASS")
+    if (type == "ATOMIC_MASS")
         return Unit::ATOMIC_MASS;
-    if(type == "ELECTRON_MASS")
+    if (type == "ELECTRON_MASS")
         return Unit::ELECTRON_MASS;
-    if(type == "POUND")
+    if (type == "POUND")
         return Unit::POUND;
-    if(type == "OUNCE")
+    if (type == "OUNCE")
         return Unit::OUNCE;
-    if(type == "TON_US")
+    if (type == "TON_US")
         return Unit::TON_US;
-    if(type == "TON_UK")
+    if (type == "TON_UK")
         return Unit::TON_UK;
-    if(type == "METRIC_TON")
+    if (type == "METRIC_TON")
         return Unit::METRIC_TON;
-    if(type == "WATT")
+    if (type == "WATT")
         return Unit::WATT;
-    if(type == "HORSEPOWER")
+    if (type == "HORSEPOWER")
         return Unit::HORSEPOWER;
-    if(type == "PASCAL")
+    if (type == "PASCAL")
         return Unit::PASCAL;
-    if(type == "HECTOPASCAL")
+    if (type == "HECTOPASCAL")
         return Unit::HECTOPASCAL;
-    if(type == "ATMOSPHERE")
+    if (type == "ATMOSPHERE")
         return Unit::ATMOSPHERE;
-    if(type == "BAR")
+    if (type == "BAR")
         return Unit::BAR;
-    if(type == "MILLIBAR")
+    if (type == "MILLIBAR")
         return Unit::MILLIBAR;
-    if(type == "MILLIMETER_OF_MERCURY")
+    if (type == "MILLIMETER_OF_MERCURY")
         return Unit::MILLIMETER_OF_MERCURY;
-    if(type == "INCH_OF_MERCURY")
+    if (type == "INCH_OF_MERCURY")
         return Unit::INCH_OF_MERCURY;
-    if(type == "GRAY")
+    if (type == "GRAY")
         return Unit::GRAY;
-    if(type == "RAD")
+    if (type == "RAD")
         return Unit::RAD;
-    if(type == "SIEVERT")
+    if (type == "SIEVERT")
         return Unit::SIEVERT;
-    if(type == "REM")
+    if (type == "REM")
         return Unit::REM;
-    if(type == "BECQUEREL")
+    if (type == "BECQUEREL")
         return Unit::BECQUEREL;
-    if(type == "CURIE")
+    if (type == "CURIE")
         return Unit::CURIE;
-    if(type == "RUTHERFORD")
+    if (type == "RUTHERFORD")
         return Unit::RUTHERFORD;
-    if(type == "ROENTGEN")
+    if (type == "ROENTGEN")
         return Unit::ROENTGEN;
-    if(type == "STERADIAN")
+    if (type == "STERADIAN")
         return Unit::STERADIAN;
-    if(type == "SPHERE")
+    if (type == "SPHERE")
         return Unit::SPHERE;
-    if(type == "KELVIN")
+    if (type == "KELVIN")
         return Unit::KELVIN;
-    if(type == "CELSIUS")
+    if (type == "CELSIUS")
         return Unit::CELSIUS;
-    if(type == "RANKINE")
+    if (type == "RANKINE")
         return Unit::RANKINE;
-    if(type == "FAHRENHEIT")
+    if (type == "FAHRENHEIT")
         return Unit::FAHRENHEIT;
-    if(type == "METRES_PER_SECOND")
+    if (type == "METRES_PER_SECOND")
         return Unit::METRES_PER_SECOND;
-    if(type == "MILES_PER_HOUR")
+    if (type == "MILES_PER_HOUR")
         return Unit::MILES_PER_HOUR;
-    if(type == "KILOMETRES_PER_HOUR")
+    if (type == "KILOMETRES_PER_HOUR")
         return Unit::KILOMETRES_PER_HOUR;
-    if(type == "KNOT")
+    if (type == "KNOT")
         return Unit::KNOT;
-    if(type == "MACH")
+    if (type == "MACH")
         return Unit::MACH;
-    if(type == "C")
+    if (type == "C")
         return Unit::C;
-    if(type == "CUBIC_METRE")
+    if (type == "CUBIC_METRE")
         return Unit::CUBIC_METRE;
-    if(type == "LITRE")
+    if (type == "LITRE")
         return Unit::LITRE;
-    if(type == "DECILITRE")
+    if (type == "DECILITRE")
         return Unit::DECILITRE;
-    if(type == "MILLILITRE")
+    if (type == "MILLILITRE")
         return Unit::MILLILITRE;
-    if(type == "CUBIC_INCH")
+    if (type == "CUBIC_INCH")
         return Unit::CUBIC_INCH;
-    if(type == "GALLON_DRY_US")
+    if (type == "GALLON_DRY_US")
         return Unit::GALLON_DRY_US;
-    if(type == "GALLON_UK")
+    if (type == "GALLON_UK")
         return Unit::GALLON_UK;
-    if(type == "OUNCE_LIQUID_UK")
+    if (type == "OUNCE_LIQUID_UK")
         return Unit::OUNCE_LIQUID_UK;
     return Unit::UNKNOWN;
 }
@@ -611,13 +612,13 @@ std::string toString(ConnectivityType conn)
 }
 ConnectivityType connectivityTypeFromString(const std::string& type)
 {
-    if(type == "MQTT")
+    if (type == "MQTT")
         return ConnectivityType::MQTT;
-    if(type == "HTTP")
+    if (type == "HTTP")
         return ConnectivityType::HTTP;
-    if(type == "AMQP")
+    if (type == "AMQP")
         return ConnectivityType::AMQP;
-    if(type == "GATEWAY")
+    if (type == "GATEWAY")
         return ConnectivityType::GATEWAY;
     return ConnectivityType::UNDEFINED;
 }
@@ -636,9 +637,9 @@ std::string toString(OutboundDataMode mode)
 }
 OutboundDataMode outboundDataModeFromString(const std::string& mode)
 {
-    if(mode == "PUSH")
+    if (mode == "PUSH")
         return OutboundDataMode::PUSH;
-    if(mode == "PULL")
+    if (mode == "PULL")
         return OutboundDataMode::PULL;
     return OutboundDataMode::UNKNOWN;
 }
@@ -677,31 +678,31 @@ std::string toString(ParameterName parameterName)
 }
 ParameterName parameterNameFromString(std::string parameterName)
 {
-    if(parameterName == "CONNECTIVITY_TYPE")
+    if (parameterName == "CONNECTIVITY_TYPE")
         return ParameterName::CONNECTIVITY_TYPE;
-    if(parameterName == "OUTBOUND_DATA_MODE")
+    if (parameterName == "OUTBOUND_DATA_MODE")
         return ParameterName::OUTBOUND_DATA_MODE;
-    if(parameterName == "OUTBOUND_DATA_RETENTION_TIME")
+    if (parameterName == "OUTBOUND_DATA_RETENTION_TIME")
         return ParameterName::OUTBOUND_DATA_RETENTION_TIME;
-    if(parameterName == "MAXIMUM_MESSAGE_SIZE")
+    if (parameterName == "MAXIMUM_MESSAGE_SIZE")
         return ParameterName::MAXIMUM_MESSAGE_SIZE;
-    if(parameterName == "FILE_TRANSFER_PLATFORM_ENABLED")
+    if (parameterName == "FILE_TRANSFER_PLATFORM_ENABLED")
         return ParameterName::FILE_TRANSFER_PLATFORM_ENABLED;
-    if(parameterName == "FILE_TRANSFER_URL_ENABLED")
+    if (parameterName == "FILE_TRANSFER_URL_ENABLED")
         return ParameterName::FILE_TRANSFER_URL_ENABLED;
-    if(parameterName == "FIRMWARE_UPDATE_ENABLED")
+    if (parameterName == "FIRMWARE_UPDATE_ENABLED")
         return ParameterName::FIRMWARE_UPDATE_ENABLED;
-    if(parameterName == "FIRMWARE_UPDATE_CHECK_TIME")
+    if (parameterName == "FIRMWARE_UPDATE_CHECK_TIME")
         return ParameterName::FIRMWARE_UPDATE_CHECK_TIME;
-    if(parameterName == "FIRMWARE_VERSION")
+    if (parameterName == "FIRMWARE_VERSION")
         return ParameterName::FIRMWARE_VERSION;
-    if(parameterName == "GATEWAY")
+    if (parameterName == "GATEWAY")
         return ParameterName::GATEWAY;
-    if(parameterName == "GATEWAY_PARENT")
+    if (parameterName == "GATEWAY_PARENT")
         return ParameterName::GATEWAY_PARENT;
-    if(parameterName == "EXTERNAL_ID")
+    if (parameterName == "EXTERNAL_ID")
         return ParameterName::EXTERNAL_ID;
-    return  ParameterName::UNKNOWN;
+    return ParameterName::UNKNOWN;
 }
 
 std::string toString(MessageType type)
@@ -763,53 +764,53 @@ std::string toString(MessageType type)
 
 MessageType messageTypeFromString(const std::string& type)
 {
-    if(type == "feed_values")
+    if (type == "feed_values")
         return MessageType::FEED_VALUES;
-    if(type == "pull_feed_values")
+    if (type == "pull_feed_values")
         return MessageType::PULL_FEED_VALUES;
-    if(type == "feed_registration")
+    if (type == "feed_registration")
         return MessageType::FEED_REGISTRATION;
-    if(type == "feed_removal")
+    if (type == "feed_removal")
         return MessageType::FEED_REMOVAL;
-    if(type == "attribute_registration")
+    if (type == "attribute_registration")
         return MessageType::ATTRIBUTE_REGISTRATION;
-    if(type == "parameters")
+    if (type == "parameters")
         return MessageType::PARAMETER_SYNC;
-    if(type == "pull_parameters")
+    if (type == "pull_parameters")
         return MessageType::PULL_PARAMETERS;
-    if(type == "time")
+    if (type == "time")
         return MessageType::TIME_SYNC;
-    if(type == "file_upload_init")
+    if (type == "file_upload_init")
         return MessageType::FILE_UPLOAD_INIT;
-    if(type == "file_upload_status")
+    if (type == "file_upload_status")
         return MessageType::FILE_UPLOAD_STATUS;
-    if(type == "file_upload_abort")
+    if (type == "file_upload_abort")
         return MessageType::FILE_UPLOAD_ABORT;
-    if(type == "file_binary_request")
+    if (type == "file_binary_request")
         return MessageType::FILE_BINARY_REQUEST;
-    if(type == "file_binary_response")
+    if (type == "file_binary_response")
         return MessageType::FILE_BINARY_RESPONSE;
-    if(type == "file_url_download_init")
+    if (type == "file_url_download_init")
         return MessageType::FILE_URL_DOWNLOAD_INIT;
-    if(type == "file_url_download_abort")
+    if (type == "file_url_download_abort")
         return MessageType::FILE_URL_DOWNLOAD_ABORT;
-    if(type == "file_url_download_status")
+    if (type == "file_url_download_status")
         return MessageType::FILE_URL_DOWNLOAD_STATUS;
-    if(type == "file_list")
+    if (type == "file_list")
         return MessageType::FILE_LIST;
-    if(type == "file_delete")
+    if (type == "file_delete")
         return MessageType::FILE_DELETE;
-    if(type == "file_purge")
+    if (type == "file_purge")
         return MessageType::FILE_PURGE;
-    if(type == "firmware_update_install")
+    if (type == "firmware_update_install")
         return MessageType::FIRMWARE_UPDATE_INSTALL;
-    if(type == "firmware_update_status")
+    if (type == "firmware_update_status")
         return MessageType::FIRMWARE_UPDATE_STATUS;
-    if(type == "firmware_update_abort")
+    if (type == "firmware_update_abort")
         return MessageType::FIRMWARE_UPDATE_ABORT;
-    if(type == "gateway_device_registration")
+    if (type == "gateway_device_registration")
         return MessageType::GATEWAY_DEVICE_REGISTRATION;
-    if(type == "gateway_device_removal")
+    if (type == "gateway_device_removal")
         return MessageType::GATEWAY_DEVICE_REMOVAL;
     return MessageType::UNKNOWN;
 }
@@ -906,6 +907,6 @@ const unsigned long long int& Reading::getTimestamp() const
 
 void Reading::setTimestamp(std::string timestamp)
 {
-    m_timestamp = std::strtoull(timestamp.c_str(), nullptr,10);
+    m_timestamp = std::strtoull(timestamp.c_str(), nullptr, 10);
 }
 }    // namespace wolkabout
