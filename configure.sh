891ecfe50f1cb5ed337930eec48d1077a58838a0
nanavuletic <nenad.vuletic@wolkabout.com>
2021-11-16 16:19:28 +0100

    Swap company name in scripts

--- configure.sh
+++ configure.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-# Copyright 2021 Adriateh d.o.o.
+# Copyright 2021 Wolkabout d.o.o.
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
